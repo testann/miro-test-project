d488b0ec93d26833125c61b453c7e4e170b49b7d
anna.saklakova <saklakova.anya@gmail.com>
2020-10-16 21:27:55 +0300

    www

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,106 +1,169 @@
 package pages;
 
+
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+/**
+	*
+	*/
 public class LoginPage extends BasePage {
 
-    private static final String URL = "https://miro.com/login/";
+	private static final String URL = "https://miro.com/login/";
 
-    @FindBy(id = "email")
-    private WebElement emailInputElement;
-    @FindBy(id = "password")
-    private WebElement passwordInputElement;
-    @FindBy(css = "button[data-autotest-id='mr-form-login-btn-signin-1']")
-    private WebElement signInButton;
-    @FindBy(css = "div[data-autotest-id='mr-error-please-enter-your-password.-1']")
-    private WebElement errorMessageWhenEmptyPassword;
-    @FindBy(css = "div[data-autotest-id='mr-error-please-enter-your-email-address.-1']")
-    private WebElement errorMessageWhenEmptyEmail;
-    @FindBy(css = "div[data-autotest-id^='mr-error-the-email-or-password-you-entered-is-incorrect']")
-    private WebElement errorMessageWhenIncorrectEmailOrPassword;
-    @FindBy(css = ".g-recaptcha")
-    private WebElement recaptcha;
 
-    public void open() {
+	//@FindBy(id = "email")
+	@FindBy(xpath = "//*")
+	private WebElement test1;
+	@FindBy(xpath = "//title[@*]")
+	private WebElement test2;
+	@FindBy(xpath = "//div | //div[@lang='en']\n")
+	private WebElement test3;
+	@FindBy(xpath = ".//*[@id='w3c_home_upcoming_events']/ul/li/div" +	"/p[2]")
+	private WebElement test4;
+	@FindBy(xpath = "/projects/project[@name='test']/editions" +	"/edition/text()")
+	private WebElement emailInputElement;
+	@FindBy(xpath = "//div[contains(@class, 'qwe')]")
+	//@FindBy(id = "password")
+	private WebElement passwordInputElement;
+	@FindBy(xpath = "child::*/child::text()")
+	// @FindBy(css = "button[data-autotest-id='mr-form-login-btn-signin-1']")
+	private WebElement signInButton;
 
-        this.driver.get(URL);
+	@FindBy(css = ".g-recaptcha")
+	private WebElement recaptcha;
 
-    }
+	@FindBy(xpath = "//tagname[@class='value']")
+	private WebElement recaptcha1;
 
-    public LoginPage enterEmail(String email) {
+	@FindBy(xpath = "count(/ta:someElement/ta:otherElement)")
+	private WebElement recaptcha2;
 
-        emailInputElement.sendKeys(email);
-        return this;
+	@FindBy(xpath = "//*[contains(text(),'here')]")
+	private WebElement recaptcha3;
 
-    }
+	@FindBy(xpath = "//*[contains(@type, '')]")
+	private WebElement recaptcha4;
 
-    public LoginPage enterPassword(String password) {
+	@FindBy(xpath = "//*[@class='fea_ee-ee']//*[text()='Test']")
+	private WebElement recaptcha5;
 
-        passwordInputElement.sendKeys(password);
-        return this;
+	@FindBy(css = "div[data-autotest-id='mr-error-please-enter-your-email-address.-1']")
+	private WebElement errorMessageWhenEmptyEmail2;
+	@FindBy(css = "div[data-autotest-id^='mr-error-the-email-or-password-you-entered-is-incorrect']")
+	private WebElement errorMessageWhenIncorrectEmailOrPassword;
 
-    }
 
-    public LoginPage enterEmailAndPassword(String login, String password) {
+	@FindBy(xpath = "//div[contains(@class, 'signup__error-item')]")
+	private WebElement errorMessageWhenEmptyEmail;
 
-        enterEmail(login).enterPassword(password);
-        return this;
+	@FindBy(css = "div[data-autotest-id='mr-error-please-enter-your-password.-1']")
+	private WebElement errorMessageWhenEmptyPassword;
 
-    }
 
-    public void clickSignInButton() {
+	public LoginPage() {
+	}
 
-        signInButton.click();
+	public void open() {
 
-    }
+		this.driver.get(URL);
 
-    public void clickSignInButtonExpectingFailure() {
+	}
 
-        clickSignInButton();
+	public LoginPage enterEmail(String email) {
 
-    }
+		emailInputElement.sendKeys(email);
+		return this;
 
-    public boolean displayErrorMessageWhenEmptyPassword() {
+	}
 
-        return waitUntilPageLoaded(errorMessageWhenEmptyPassword).isDisplayed();
+	//review
 
-    }
 
-    public boolean displayErrorMessageWhenEmptyEmail() {
+	public LoginPage enterPassword(String p) {
 
-        return  waitUntilPageLoaded(errorMessageWhenEmptyEmail).isDisplayed();
+		String str;
+		passwordInputElement.sendKeys(p);
+		return this;
 
-    }
+	}
 
-    public boolean displayErrorMessageWhenIncorrectEmailOrPassword() {
+	public LoginPage enterEmailAndPassword(String login, String password) {
 
-        try {
+		enterEmail(login).enterPassword(password);
+		return this;
 
-            return waitUntilPageLoaded(errorMessageWhenIncorrectEmailOrPassword).isDisplayed();
+	}
 
-        } catch(TimeoutException exception) {
+	public void clickSignInButton() {
 
-            return recaptcha.isDisplayed();
+		signInButton.click();
 
-        }
+	}
 
-    }
+	public void clickSignInButtonExpectingFailure() {
 
-    public DashboardPage submitLogin() {
+		clickSignInButton();
 
-        clickSignInButton();
-        return new DashboardPage();
 
-    }
+	}
 
-    public LoginPage clearInputs() {
+	public boolean displayErrorMessageWhenEmptyPassword() {
 
-        emailInputElement.clear();
-        passwordInputElement.clear();
-        return this;
+		return waitUntilPageLoaded(errorMessageWhenEmptyPassword).isDisplayed();
 
-    }
+	}
+
+	public boolean displayErrorMessageWhenEmptyEmail() {
+
+		return waitUntilPageLoaded(errorMessageWhenEmptyEmail).isDisplayed();
+
+	}
+
+	public boolean displayErrorMessageWhenIncorrectEmailOrPassword() {
+
+		try {
+
+			return waitUntilPageLoaded(errorMessageWhenIncorrectEmailOrPassword).isDisplayed();
+
+		} catch (TimeoutException exception) {
+
+			return recaptcha.isDisplayed();
+
+		}
+
+	}
+
+	public DashboardPage submitLogin() {
+
+		DashboardPage d = new DashboardPage();
+
+		clickSignInButton();
+		//d = new DashboardPage();
+		return d;
+
+
+	}
+
+	public LoginPage clearInputs() {
+
+		emailInputElement.clear();
+		passwordInputElement.clear();
+		return this;
+		//tram papm pam to check
+
+	}
+
+	/**
+		* {@code some code}. bla bla.
+		*/
+	private void method() {
+
+		System.out.println("div pre .aaa{border: 1px;}");
+	}
+
 
 }
+
+
